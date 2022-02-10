d4f0fb6b04ab620858c1a67f9bb90adc250531d1
ctm1098 <carlosbond1998@gmail.com>
2022-02-10 18:50:30 +0100

    mouse.py con help

--- code/mouse.py
+++ code/mouse.py
@@ -2,25 +2,45 @@
 
 import cv2 as cv
 from umucv.stream import autoStream
-from umucv.util import putText
+from umucv.util   import   putText
+from collections  import  deque
+from umucv.util import Help
 
-points = []
+help = Help(
+"""
+HELP WINDOW
+
+Left-click: draw circle
+Right-Click: clear
+
+SPC: pausa
+ESC: exit
+
+h: show/hide help
+""")
+
+points = deque([], maxlen = 6)
 
 def fun(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
         print(x,y)
         points.append((x,y))
+    elif event == cv.EVENT_RBUTTONDOWN:
+        points.clear()
+        print("CLEARED")
 
+    
 cv.namedWindow("webcam")
 cv.setMouseCallback("webcam", fun)
 
 
-def drawCircle(points, img):
+def drawCircle(points, frame):
     for p in points:
         cv.circle(frame,p, 10, (0,0,255), -1)
 
-def drawDistances(points,img):
-    for (p1,p2) in zip(points,points[1:]):
+def drawDistances(points,frame):
+    l = list(points)
+    for (p1,p2) in zip(l,l[1:]):
         cv.line(frame,p1,p2,(0,255,0))
         x1,y1 = p1
         x2,y2 = p2
@@ -30,6 +50,7 @@ def drawDistances(points,img):
 
 
 for key, frame in autoStream():
+    help.show_if(key, ord('h'))
     drawCircle(points,frame)
     drawDistances(points,frame)
     cv.imshow('webcam',frame)
