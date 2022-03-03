ea6cce5d9cfece1e372e05eb9223ef6ddc17b438
ctm1098 <carlosbond1998@gmail.com>
2022-03-03 18:30:02 +0100

    modificaciones sobre COLOR

--- MIS SCRIPTS/CALIBRACION.py
+++ MIS SCRIPTS/CALIBRACION.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
 import argparse
-import cv2 as cv
-import numpy as np
-from umucv.util   import   putText
-from collections  import  deque
-from umucv.util import Help
-from math       import pi
+import cv2         as cv
+import numpy       as np
+from   umucv.util  import putText
+from   collections import deque
+from   umucv.util  import Help
+from   math        import pi
 
 
 parser = argparse.ArgumentParser(description='Mide la distancia en pixels entre 2 puntos seleccionados')
 parser.add_argument('-im', metavar='--image', type=str, required=True,
                     help='La imagen con la que tratar')
 parser.add_argument('-f', metavar='--focalLength', type=float, required=False, default = 775.5211,
-                    help='La distancia focal medida en pixels')
+                    help='La distancia focal medida en pixels (por defecto, 775.52')
 args = parser.parse_args()
 print(vars(args))
 
@@ -22,17 +22,17 @@ def readrgb(file):
     return cv.cvtColor( cv.resize(cv.imread(file), (1000,500)), cv.COLOR_BGR2RGB)
 
 def manejador(event, x, y, flags, param):
-    global points, clicked
+    global points, modified
     if event == cv.EVENT_LBUTTONDOWN:
         print(x,y) 
         points.append((x,y))
-        clicked = True
     elif event == cv.EVENT_RBUTTONDOWN:
         points.clear()
         print("CLEARED")
-        clicked = False
         global image, original_image
         image = original_image.copy()
+    
+    modified = True
 
 
 def drawCircle(points, frame):
@@ -60,7 +60,6 @@ clicked = False
 image = readrgb(args.im)
 original_image = image.copy()
 print(image.shape)
-cont = True
 modified = True
 
 while cont:
@@ -71,19 +70,13 @@ while cont:
     if key == 27:
         cont = False
     
-    else:
-        # Con esta comprobacion evitamos dibujar los mismos circulos mas de una vez
-        if clicked:
-            image = original_image.copy();
-            drawCircle(points,image)
-            drawDistancesAndAngles(points,image)
-            drawDistancesAndAngles(points,image)
-            modified = True
-            clicked = False    
-
+    elif modified:
+        image = original_image.copy();
+        drawCircle(points,image)
+        drawDistancesAndAngles(points,image)
+        drawDistancesAndAngles(points,image)
         # No dibujamos la imagen a no ser que haya cambios o sea la primera vez que la mostramos
-        if (modified):
-            cv.imshow('image',image)
-            modified = False
+        cv.imshow('image',image)
+        modified = False
 
 cv.destroyAllWindows()
--- MIS SCRIPTS/COLOR.py
+++ MIS SCRIPTS/COLOR.py
@@ -1,33 +1,40 @@
 import argparse
 import cv2               as     cv
 import numpy             as     np
-import matplotlib.pyplot as plt
-from   umucv.util        import ROI
+from   umucv.util        import ROI, putText
 from   collections       import deque
 from   sklearn.metrics   import mean_squared_error
-
-
-def readrgb(file):
-    return cv.cvtColor( cv.resize(cv.imread(file), (1000,500)), cv.COLOR_BGR2RGB)
-
-def calcNormHistRGB(img):
-    '''Dada una imagen RGB devuelve las frecuencias normalizadas de cada valor de pixel para cada canal de color '''
-    hR,_ = np.histogram(img[:,:,0], np.arange(0, 256)) 
-    hG,_ = np.histogram(img[:,:,1], np.arange(0, 256))
-    hB,_ = np.histogram(img[:,:,2], np.arange(0, 256))
-    return hR/np.sum(hR), hG/np.sum(hG), hB/np.sum(hB)
-
-def adjustROI(im):
-    return cv.resize(im, (333,300))
 
 parser = argparse.ArgumentParser(description='Clasificación de imagen en base a ROIs como modelos')
 parser.add_argument('-im', metavar='--image', type=str, required=True,
                     help='La imagen con la que tratar')
 parser.add_argument('-models', metavar='--models_count', type=int, default=3,
                     help='El máximo número de modelos permitidos (por defecto, 3)')
+parser.add_argument('-bins', metavar='--image', type=int, required=False, default = 8,
+                    help='El numero de bins en los que dividir el rango de valores del hisograma (por defecto, 8)')
 
 args = parser.parse_args()
 print(vars(args))
+
+def readrgb(file):
+    return cv.cvtColor( cv.resize(cv.imread(file), (1000,500)), cv.COLOR_BGR2RGB)
+
+def calcHistRGB(img):
+    '''Dada una imagen RGB devuelve las frecuencias de cada valor de pixel para cada canal de color '''
+    R = np.histogram(img[:,:,0], args.bins, range = (0,256)) 
+    G = np.histogram(img[:,:,1], args.bins, range = (0,256))
+    B = np.histogram(img[:,:,2], args.bins, range = (0,256))
+    return R,G,B
+
+def adjustROI(im):
+    return cv.resize(im, (333,300))
+
+def adjustLimit(hists, bins, limit, limitW):
+    max = np.max([np.max(h) for h in hists])
+    t = tuple([h*limit/max for h in hists])
+    bins = [b * limitW/255 for b in bins]
+    return (bins,t[0]), (bins,t[1]), (bins,t[2])
+
 
 
 cv.namedWindow("image")
@@ -53,11 +60,31 @@ cont = True
 while cont:
     if region.roi:
         [x1,y1,x2,y2] = region.roi
+        putText(img, f'{x2-x1+1}x{y2-y1+1}', orig=(x1,y1-8))
         # prevDims son las dimensiones del ROI en iteracion previa, de esta forma nos evitamos leer y dibujar el ROI mas veces de las necesarias
         if (prevDims != [x1,y1,x2,y2]):
             prevDims = [x1,y1,x2,y2]
             img = original_img.copy()
-            cv.rectangle(img, (x1,y1), (x2,y2), color=(0,255,255), thickness=2)
+            model = img[y1:y2+1, x1:x2+1]
+            hR,hG,hB = calcHistRGB(model)
+            print("=========== HR ===============")
+            print(hR)
+            print("==============================")
+            print("=========== HG ===============")
+            print(hG)
+            print("==============================")
+            print("=========== HB ===============")
+            print(hB)
+            print("==============================")
+            hR2, hG2, hB2 = adjustLimit([hR[0],hG[0],hB[0]],hR[1],abs(y1-y2+1), abs(x1-x2+1))
+            print(hR2)
+            draw_pointsR = np.array(list(zip(hR2[0], hR2[1]))).astype(np.int32)
+            draw_pointsG = np.array(list(zip(hG2[0], hG2[1]))).astype(np.int32)
+            draw_pointsB = np.array(list(zip(hB2[0], hB2[1]))).astype(np.int32)
+            cv.rectangle(img, (x1,y1), (x2,y2), color=(0,255,255), thickness = 2)
+            cv.polylines(model, [draw_pointsR], False, (255,0,0), thickness = 2)  # args: image, points, closed, color
+            cv.polylines(model, [draw_pointsG], False, (0,255,0), thickness = 2)
+            cv.polylines(model, [draw_pointsB], False, (0,0,255), thickness = 2)
 
     key = cv.waitKey(1)
 
@@ -67,14 +94,14 @@ while cont:
         model = adjustROI(original_img[y1:y2+1, x1:x2+1])
         models.append(model)
         # En la lista de histogramas guardamos los histogramas de cada canal de una imagen como una tupla (R,G,B)
-        histograms.append((calcNormHistRGB(model)))
+        histograms.append((calcHistRGB(model)))
 
 
     # Seleccionar el modelo mas parecido al ROI indicado
     # Tecla espacio
     if key == 32 and models:
-        """ piece = adjustROI(original_img[y1:y2+1, x1:x2+1])
-        R,G,B = calcNormHistRGB(piece)
+        piece = adjustROI(original_img[y1:y2+1, x1:x2+1])
+        R,G,B = calcHistRGB(piece)
         rgbConcat = np.concatenate((R,G,B))
 
         mseList = []
@@ -84,9 +111,9 @@ while cont:
             mse = mean_squared_error(rgbConcat, histContat)
             mseList.append(mse)
             print("MSE con el modelo " + str(it) +" = " + str(mse))
-            it += 1 """
+            it += 1
 
-        mseList = [mean_squared_error(np.concatenate((calcNormHistRGB(original_img[y1:y2+1, x1:x2+1]))), np.concatenate((hR,hG,hB))) for (hR,hG,hB) in histograms]
+        # mseList = [mean_squared_error(np.concatenate((calcNormHistRGB(original_img[y1:y2+1, x1:x2+1]))), np.concatenate((hR,hG,hB))) for (hR,hG,hB) in histograms]
 
         detected.append(models[np.argmin(mseList)])
 
